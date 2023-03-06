d312c26cc90eb958ec35560ef754079863939852
bisco9917 <121897095+bisco9917@users.noreply.github.com>
2023-03-05 18:14:08 -0800

    Refactor code (improve resource names etc)
    I had originally planned to use Salt to control the instances (as a 
    demonstration), but I decided this would be pretty redundant for what I
    am trying to do and might be confusing to the reviewer.  I may add
    something like this in the future but for now I will not use Salt.
    
    - Replace "salt" with "js" as a resource name differentiator.
    - Improve several resource names for clarity.
    - Renamed environments.tf to ec2.tf

--- vpc.tf
+++ vpc.tf
@@ -2,33 +2,33 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "salt_environment" {
+resource "aws_vpc" "js_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.salt_environment.id
+resource "aws_internet_gateway" "js_internet_gateway" {
+  vpc_id = aws_vpc.js_vpc.id
 }
 
-resource "aws_subnet" "salt_public_subnet" {
-  vpc_id                  = aws_vpc.salt_environment.id
+resource "aws_subnet" "js_subnet" {
+  vpc_id                  = aws_vpc.js_vpc.id
   cidr_block              = "10.0.${1 + count.index}.0/24"
   count                   = length(data.aws_availability_zones.available.names)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.salt_environment.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.js_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.js_internet_gateway.id
   }
 }
 
-resource "aws_route_table_association" "public_rta" {
+resource "aws_route_table_association" "public_route_table_association" {
   count          = length(data.aws_availability_zones.available.names)
-  subnet_id      = element(aws_subnet.salt_public_subnet.*.id, count.index)
-  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = element(aws_subnet.js_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
 }
